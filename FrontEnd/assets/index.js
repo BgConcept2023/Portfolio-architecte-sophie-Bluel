dff20dac6e14c08f4d14107f3f35902fe4649e10
Bruno@Garcia <bgconcept.web@gmail.com>
2024-05-24 18:56:59 +0200

    Modif. index.html

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -3,6 +3,6 @@ let works = [];
 async function loadWorks(){
     const response = await fetch("http://localhost:5678/api/works");
    let works = await response.json();
-    console.log(works[1]);
+    console.log(works);
 }
 loadWorks();
