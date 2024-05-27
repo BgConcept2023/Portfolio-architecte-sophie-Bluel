e156496a0ab78f8ef616c0410e53872fd0642df2
Bruno@Garcia <146881476+BgConcept2023@users.noreply.github.com>
2024-05-27 09:58:50 +0200

    affichage des images

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,12 +1,39 @@
 //Variables générales
 let works = []; //tableau
 
+const baliseGallery = document.querySelector(".gallery");//sélection de la classe gallery
+
 //Récupérationd du ripos sur swagger avec la fonction loadWorks
 async function loadWorks(){
-    const response = await fetch("http://localhost:5678/api/works");
+   const response = await fetch("http://localhost:5678/api/works");
    let works = await response.json();
-    console.log(works);
+    
+   console.log(works);
+    
+    //Affichage
+    affichageGallery(works);
 }
 loadWorks();
 
 //Création des balises et affichage des images.
+
+function affichageGallery(filterWorks){  
+    for(let index = 0; index < filterWorks.length; index++ ){
+
+const baliseFigure = document.createElement("figure");
+const baliseImage = document.createElement("img");
+const baliseFigcaption = document.createElement("figcaption");
+
+baliseGallery.appendChild(baliseFigure);
+baliseFigure.appendChild(baliseImage);
+baliseFigure.appendChild(baliseFigcaption);
+
+baliseFigcaption.innerHTML = filterWorks[index].title;
+baliseImage.src = filterWorks[index].imageUrl;
+
+console.log(baliseGallery);
+console.log(baliseFigure);
+
+
+    }
+}
