03170cc71d3888c0c53e67c0e744dcfbf1d55227
Bruno@Garcia <146881476+BgConcept2023@users.noreply.github.com>
2024-05-27 09:22:21 +0200

    ajout des dif. functions

--- FrontEnd/assets/index.js
+++ FrontEnd/assets/index.js
@@ -1,8 +1,12 @@
-let works = [];
+//Variables générales
+let works = []; //tableau
 
+//Récupérationd du ripos sur swagger avec la fonction loadWorks
 async function loadWorks(){
     const response = await fetch("http://localhost:5678/api/works");
    let works = await response.json();
     console.log(works);
 }
 loadWorks();
+
+//Création des balises et affichage des images.
