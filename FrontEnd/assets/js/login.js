a7da9594a5c4f0f66005fd3d03833602f3247e13
Bruno@Garcia <bgconcept.web@gmail.com>
2024-06-13 10:34:52 +0200

    début du code js pour le login

---
+++ FrontEnd/assets/js/login.js
@@ -0,0 +1,6 @@
+// Déclaration des variables
+const email = document.querySelector("form #email");
+const password = document.querySelector("form #password");
+const form = document.querySelector("form");
+const messageError = document.querySelector("#contact-2 p");
+//  console.log(email, password, form, messageError);
