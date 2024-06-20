5b303432d9e7e294d1b410723b0dc76c0d7a1127
Bruno@Garcia <bgconcept.web@gmail.com>
2024-06-20 11:40:43 +0200

    Login et modal

--- FrontEnd/assets/js/login.js
+++ FrontEnd/assets/js/login.js
@@ -3,20 +3,43 @@ const form = document.querySelector("form");
 const email = document.querySelector("form #email");
 const password = document.querySelector("form #password");
 const messageError = document.querySelector("#contact-2 p");
-const login = Document.querySelector("#login");
+const login = document.querySelector("#login");
 //  console.log(email, password, form, messageError);
 
 //ne pas recharger la page
 form.addEventListener('submit',(event)=>{
     event.preventDefault();
+   const data = {email:email.value, password:password.value};
+
+   
+    postJSON(data)
 })
 
 //récupération de l'authentification 
-async function loadAuthentification(authentification){
 
-    const response = await fetch("http://localhost:5678/api/users/login");
-    authentification = await response(body);
+    async function postJSON(data) {
+        try {
+          const response = await fetch("http://localhost:5678/api/users/login", {
+            method: "POST", // or 'PUT'
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          });
+      
+          const result = await response.json();
+          const token = result.token;
+          localStorage.setItem("token",token);
+          console.log("Success:", result);
+        } catch (error) {
+          console.error("Error:", error);
+        }
+      }
 
-};
+      
+      
+
+     
+
 
 // à finir
---
+++ FrontEnd/assets/js/modal.js
@@ -0,0 +1,41 @@
+
+const containerGlobal = document.createElement("aside");
+containerGlobal.className = "container_global";
+
+const modalPhotos = document.createElement("div");
+modalPhotos.className = "modal_photos";
+
+const elementH2 = document.createElement("h2");
+elementH2.textContent ="Galerie photos";
+
+const faSolid = document.createElement("i");
+faSolid.className = "fa-solid fa-xmark";
+const miniGallery = document.createElement("div");
+miniGallery.className = "mini-galerie";
+
+const  btnAjout = document.createElement("button");
+btnAjout.className = "btn_submit";
+btnAjout.type = "submit";
+btnAjout.textContent = "Ajouter une photo";
+
+// console.log(modalPhotos, faSolid, elementH2, btnAjout);
+
+//Insertion des élemnts dans le html
+
+document.body.appendChild(containerGlobal);
+containerGlobal.appendChild(modalPhotos);
+modalPhotos.appendChild(faSolid);
+modalPhotos.appendChild(elementH2);
+modalPhotos.appendChild(miniGallery);
+modalPhotos.appendChild(btnAjout);
+
+//affichage modal au click
+const containerAjout = document.querySelector(".container_global");
+const buttonEdit = document.querySelector("#btn_Click");
+const buttonAjout = document.querySelector(".btn_submit");
+
+
+buttonAjout.addEventListener("click",()=>{
+   containerAjout.classList.add("container_global");
+console.log("ok")
+})
